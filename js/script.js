37f92d98d4ab0b24bb59a4dc9ebe9c531e9cd4ac
Ravi Gupta <ravigupta35516@gmail.com>
2021-10-20 12:22:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// --------Change nav-btn and open navigation----------
 document.querySelector(".nav-btn").addEventListener("click", function () {
   if (document.querySelector(".nav").style.display == "none") {
     document.querySelector(".nav").style.display = "block";
@@ -8,6 +9,16 @@ document.querySelector(".nav-btn").addEventListener("click", function () {
 }
 });
 
+// ------Close Nav when clicking on link------
+let listItem = document.querySelectorAll(".list-item");
+for(let i=0; i <= listItem.length; i++){
+    listItem[i].addEventListener("click", function(){
+        document.querySelector(".nav").style.display = "none";
+        document.querySelector("span").classList.remove("active");
+    });
+}
+
+// -------Close div when clicking on overlay--------
 document.querySelector(".overlay").addEventListener("click", function () {
     document.querySelector("span").classList.remove("active");
     document.querySelector(".nav").style.display = "none";
